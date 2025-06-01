fa0d498796e1a6c5583f2e31e8f251777e9640f7
Yulia  Demir <Nightelfin91@gmail.com>
2025-06-01 11:41:20 +0300

    fix: lint errors

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Delete, Get, HttpCode, Param, ParseUUIDPipe, Post } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  ParseUUIDPipe,
+  Post,
+} from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
 import { ArtistsService } from 'src/artists/artists.service';
 import { AlbumsService } from 'src/albums/albums.service';
@@ -6,99 +14,105 @@ import { TracksService } from 'src/tracks/tracks.service';
 
 @Controller('favs')
 export class FavoritesController {
-    constructor(
-        private readonly favoritesService: FavoritesService,
-        private readonly artistsService: ArtistsService,
-        private readonly albumsService: AlbumsService,
-        private readonly tracksService: TracksService,
-    ) {}
+  constructor(
+    private readonly favoritesService: FavoritesService,
+    private readonly artistsService: ArtistsService,
+    private readonly albumsService: AlbumsService,
+    private readonly tracksService: TracksService,
+  ) {}
 
-    @Get()
-    getAll() {
-        return this.favoritesService.findAll();
-    }
+  @Get()
+  getAll() {
+    return this.favoritesService.findAll();
+  }
 
-    //TRACKS
-    @Post('track/:id')
-    addTrackToFavorites(
-        @Param(
-            'id', 
-            new ParseUUIDPipe({
-                version: '4',
-                errorHttpStatusCode: 400,
-            }),
-        ) id: string
-    ) {
-        this.favoritesService.addTrack(id);
-    }
+  //TRACKS
+  @Post('track/:id')
+  addTrackToFavorites(
+    @Param(
+      'id',
+      new ParseUUIDPipe({
+        version: '4',
+        errorHttpStatusCode: 400,
+      }),
+    )
+    id: string,
+  ) {
+    this.favoritesService.addTrack(id);
+  }
 
-    @Delete('track/:id')
-    @HttpCode(204)
-    deleteTrackToFavorites(
-        @Param(
-            'id', 
-            new ParseUUIDPipe({
-                version: '4',
-                errorHttpStatusCode: 400,
-            }),
-        ) id: string
-    ) {
-        this.favoritesService.removeTrack(id);
-    }
+  @Delete('track/:id')
+  @HttpCode(204)
+  deleteTrackToFavorites(
+    @Param(
+      'id',
+      new ParseUUIDPipe({
+        version: '4',
+        errorHttpStatusCode: 400,
+      }),
+    )
+    id: string,
+  ) {
+    this.favoritesService.removeTrack(id);
+  }
 
-    //ARTISTS
-    @Post('artist/:id')
-    addArtistToFavorites(
-        @Param(
-            'id', 
-            new ParseUUIDPipe({
-                version: '4',
-                errorHttpStatusCode: 400,
-            }),
-        ) id: string
-    ) {
-        this.favoritesService.addArtist(id);
-    }
+  //ARTISTS
+  @Post('artist/:id')
+  addArtistToFavorites(
+    @Param(
+      'id',
+      new ParseUUIDPipe({
+        version: '4',
+        errorHttpStatusCode: 400,
+      }),
+    )
+    id: string,
+  ) {
+    this.favoritesService.addArtist(id);
+  }
 
-    @Delete('artist/:id')
-    @HttpCode(204)
-    deleteArtistFromFavorites(
-        @Param(
-            'id', 
-            new ParseUUIDPipe({
-                version: '4',
-                errorHttpStatusCode: 400,
-            }),
-        ) id: string
-    ) {
-        this.favoritesService.removeArtist(id);
-    }
+  @Delete('artist/:id')
+  @HttpCode(204)
+  deleteArtistFromFavorites(
+    @Param(
+      'id',
+      new ParseUUIDPipe({
+        version: '4',
+        errorHttpStatusCode: 400,
+      }),
+    )
+    id: string,
+  ) {
+    this.favoritesService.removeArtist(id);
+  }
 
-    //ALBUMS
-    @Post('album/:id')
-    addAlbumToFavorites(
-        @Param(
-            'id', 
-            new ParseUUIDPipe({
-                version: '4',
-                errorHttpStatusCode: 400,
-            }),
-        ) id: string
-    ) {
-        this.favoritesService.addAlbum(id);
-    }
+  //ALBUMS
+  @Post('album/:id')
+  addAlbumToFavorites(
+    @Param(
+      'id',
+      new ParseUUIDPipe({
+        version: '4',
+        errorHttpStatusCode: 400,
+      }),
+    )
+    id: string,
+  ) {
+    this.favoritesService.addAlbum(id);
+  }
 
-    @Delete('album/:id')
-    @HttpCode(204)
-    deleteAlbumFromFavorites(
-        @Param(
-            'id', 
-            new ParseUUIDPipe({
-                version: '4',
-                errorHttpStatusCode: 400,
-            }),
-        ) id: string
-    ) {
-        this.favoritesService.removeAlbum(id);
-    }
+  @Delete('album/:id')
+  @HttpCode(204)
+  deleteAlbumFromFavorites(
+    @Param(
+      'id',
+      new ParseUUIDPipe({
+        version: '4',
+        errorHttpStatusCode: 400,
+      }),
+    )
+    id: string,
+  ) {
+    this.favoritesService.removeAlbum(id);
+  }
 }
--- src/tracks/tracks.module.ts
+++ src/tracks/tracks.module.ts
@@ -6,6 +6,6 @@ import { TracksService } from './tracks.service';
   imports: [TracksModule],
   controllers: [TracksController],
   providers: [TracksService],
-  exports: [TracksService]
+  exports: [TracksService],
 })
 export class TracksModule {}
